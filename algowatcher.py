90572434d0f084ced9df3dfc9a69178a369578a7
Avi Misra <avislash@gmail.com>
2021-03-14 01:07:46 -0500

    Reorganized code to be cleaner/more readible and added comments

--- algowatcher.py
+++ algowatcher.py
@@ -11,31 +11,29 @@ from telegram.ext import MessageHandler, Filters
 from algosdk.v2client import algod
 from datetime import datetime
 from time import sleep
+from util import util
 
+#Global variables. These objects need to be used by the command call back functions  below
+#But there's no way of initializing them in main() and passing them in when handling
+#the commands
 algoClient = {}
 localContext = {}
-planetAssetId = 27165954
+planetAssetId = 27165954 #Asset ID for Planet ASA
 init = False;
 
+#init function used to load in values saved from the last bot session
 def _init(update, context):
    global localContext
+   global init
    if False == init:
        localContext = context
        context.bot.send_message(chat_id=update.effective_chat.id, text="Initialized")
+       init = True
 
 
-def getAssetBalance(algoAddress, assetId):
-   global algoClient
 
-   balance = 0
-   account_info = algoClient.account_info(algoAddress)
-   assets = account_info.get('assets')
-
-   for asset in assets:
-       if asset['asset-id'] == int(assetId):
-           balance = asset['amount']
-   return balance
-
+#Displays the start menu whenever user types /start in Telegram chat
+#This contains all commands available to user along with brief description
 def start(update, context):
     greeting_str = "Hello. You can type the following commands:\n\t\n"
     start_str = " /start  - Display this menu\n\t"
@@ -50,13 +48,15 @@ def start(update, context):
     monitorStatus_str = "/getMonitorStatus - Check if Planet Monitoring is enabled/disabled\n\t\n"
 
     coffe_str = "Like the bot and want to buy the developer coffe? Send an algo to ONPJRXNOOAIZVJ3VPSZKFGBMSYQRACNZUMW5P6FNQTKUZ3BALYCDBUMNAM\n\n"
-    support_str = "For Questions, Suggestions, and Feedback join the AlgoWatchers Telegram Group (telegram.me/algowatchers).\n" 
+    support_str = "For Questions, Suggestions, and Feedback join the AlgoWatchers Telegram Group (t.me/algowatchers).\n" 
 
     final_str = coffe_str + support_str
 
     message = greeting_str + start_str + address_str + getAlgoBal_str + getAssetBal_str + getPlanetBal_str + startMonitor_str + stopMonitor_str + monitorStatus_str + final_str
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
+#Sets the Algo Public Address to monitor/query balances for
+#and also initializes the user_data dictionary for the user
 def updateAddress(update, context):
     global localContext
     context.user_data[update.effective_chat.id] = {'address' : context.args[0], 'monitor' : False, 'asset': 0, 'startTime': datetime(70,1,1), 'interval': 30}
@@ -64,6 +64,23 @@ def updateAddress(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     localContext = context
 
+#Helper function that gets the current number of ASA Tokens denoted by assetId
+#in located at the public Algorand Address (algoAddress)
+def getAssetBalance(algoAddress, assetId):
+   global algoClient
+   balance = 0
+   account_info = algoClient.account_info(algoAddress)
+   assets = account_info.get('assets')
+
+   for asset in assets:
+       if asset.get('asset-id') == int(assetId):
+           balance = asset['amount']
+
+   return balance
+
+
+#Gets the current Amount of ALGO in 
+#in the wallet address that was set using /address (updateAddress)
 def getAlgoBalance(update, context):
     global algoClient
     try:
@@ -76,6 +93,8 @@ def getAlgoBalance(update, context):
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
+#Query  Algorand Addres (set by updateAddress) 
+#for current Planet Balance (Algorand ASA)
 def getPlanetBalance(update, context):
     global algoClient
     global planetAssetId
@@ -88,79 +107,25 @@ def getPlanetBalance(update, context):
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
-def monitorAsset(dispatcher):
-   global algoClient
-   global planetAssetId
-   global localContext
 
-   while True:
-       if localContext:
-           for userId in localContext.user_data:
-               user_data = localContext.user_data[userId]
-               if True == user_data.get('monitor'):
-                   time_elapsed = (datetime.now() - user_data.get('startTime')).total_seconds()
-                   if time_elapsed >= user_data.get('interval'):
-                       planetsNow = getAssetBalance(user_data.get('address'), planetAssetId)
-                       if planetsNow == user_data.get('asset'):
-                           dispatcher.bot.send_message(chat_id=userId, text="No New Planets Detected. Please Make sure your Sensor and App are still active")
-                       user_data['asset'] = planetsNow
-                       user_data['startTime'] = datetime.now()
-           sleep(1)
-
-def roundFloat(val):
-    decimal = val - int(val)
-    if 0.5 <= decimal:
-        val = math.ceil(val)
-    else:
-        val = math.floor(val)
-
-    return int(val)
-
-
-def getInterval(args):
-   MINIMUM_CHECK_INTERVAL = 30 #seconds
-   interval = MINIMUM_CHECK_INTERVAL #default monitor rate is 30 seconds
-
-   if len(args) > 0:
-       if args[0].find('s') > 0: 
-           interval = roundFloat(float(args[0].split('s')[0]))
-       elif args[0].find('m') > 0:
-           interval = args[0].split('m')[0]
-           interval = int(float(interval)*60)
-
-   interval = interval if MINIMUM_CHECK_INTERVAL < interval else MINIMUM_CHECK_INTERVAL
-
-   return interval
-
-def getIntervalUnits(interval):
-   units = "seconds"
-   if interval > 59:
-       units = "minute"
-       if (interval/60) > 1:
-           units = units + "s"
-   return units
-
-def intervalToStr(interval):
-    units = getIntervalUnits(interval)
-    if interval > 59:
-        interval = interval/60
-
-    return str(interval) + " " + units
-
+#Enable Planet monitoring for Telegram user
+#If no arguments are specified then the account will be
+#monitor at the last set interval (default 30 seconds)
 def startMonitor(update, context):
    global localContext
    if 'monitor' in context.user_data[update.effective_chat.id]:
        interval = context.user_data[update.effective_chat.id].get('interval')
        if len(context.args) > 0 :
-           interval = getInterval(context.args)
+           interval = util.getInterval(context.args)
            context.user_data[update.effective_chat.id]['interval'] = interval
        context.user_data[update.effective_chat.id]['monitor'] = True
        localContext = context
-       message = "Monitor Enabled. Monitoring every " + intervalToStr(interval)
+       message = "Monitor Enabled. Monitoring every " + util.intervalToStr(interval)
        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
    else:
        context.bot.send_message(chat_id=update.effective_chat.id, text="Unable to start monitor. Make sure you set an address with /address first")
-    
+ 
+#Disable Planet Monitoring for Telegram user
 def stopMonitor(update, context):
     global localContext
     try:
@@ -171,17 +136,25 @@ def stopMonitor(update, context):
 
     context.bot.send_message(chat_id=update.effective_chat.id, text="Monitor Stopped")
 
+#Query  Algorand Addres (set by updateAddress) 
+#for Algorand ASA Balance denoted by
+#passed in Asset ID (context.args[0]) 
 def getAssetBalanceCmd(update, context):
    try:
        assetId = context.args[0]
    except:
        context.bot.send_message(chat_id=update.effective_chat.id, text="No Asset ID Provided")
        return
+
+   #The Indexer must be used to get ASA Meta data such as unit-name and
+   #this requires an archival node. Instead of running my own archical ndoe
+   #I instead grab this information using the AlgoExploer API
    algoExplorerAssetUrl = "https://api.algoexplorer.io/idx2/v2/assets/"
    algoExplorerAssetUrl= algoExplorerAssetUrl + str(assetId)
    
    asset_info = json.loads(requests.get(algoExplorerAssetUrl).text)
 
+   #Make sure ASA ID provided is valid
    if "asset" in asset_info:
        try:
            algoAddress = context.user_data[update.effective_chat.id].get('address')
@@ -204,12 +177,14 @@ def getAssetBalanceCmd(update, context):
 
    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
+#Reports current monitor status (Enabled/Disabled) 
+#and monitor interval to Telegram user
 def getMonitorStatus(update, context):
    try:
        monitorStatus = context.user_data[update.effective_chat.id].get('monitor')
        if monitorStatus:
            interval = context.user_data[update.effective_chat.id].get('interval')
-           message = "Monitor Enabled. Monitoring every " + intervalToStr(interval)
+           message = "Monitor Enabled. Monitoring every " + util.intervalToStr(interval)
        else:
            message = "Monitor Disabled"
    except:
@@ -217,10 +192,33 @@ def getMonitorStatus(update, context):
 
    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
-
+#Used when an unregistered command comes in
 def unknown(update, context):
     message = "Unknown Command. Type /start to see list of available commands"
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+#Monitor Thread for mointoring Planets
+#Thread will report to Telegram User if no new
+#Planets came in over the set Monitor Interval
+#TODO: Update to be able to monitor any existing ASA
+def monitorAsset(dispatcher):
+   global algoClient
+   global planetAssetId
+   global localContext
+
+   while True:
+       if localContext:
+           for userId in localContext.user_data:
+               user_data = localContext.user_data[userId]
+               if True == user_data.get('monitor'):
+                   time_elapsed = (datetime.now() - user_data.get('startTime')).total_seconds()
+                   if time_elapsed >= user_data.get('interval'):
+                       planetsNow = getAssetBalance(user_data.get('address'), planetAssetId)
+                       if planetsNow == user_data.get('asset'):
+                           dispatcher.bot.send_message(chat_id=userId, text="No New Planets Detected. Please Make sure your Sensor and App are still active")
+                       user_data['asset'] = planetsNow
+                       user_data['startTime'] = datetime.now()
+           sleep(1)
 
 def main():
    global algoClient
@@ -234,7 +232,6 @@ def main():
 
    algoClient = algod.AlgodClient(algoNodeToken, algoNodeAddress)
    persist = PicklePersistence(filename='botContext.pickle')
-   bot = telegram.Bot(token=botToken)
    updater = Updater(token=botToken, persistence=persist, use_context=True)
    dispatcher = updater.dispatcher
 
